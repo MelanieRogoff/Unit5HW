b7b48ffa41a4c1c19116fe6785b0085616549cbb
Melanie Rogoff <mgrogoff@gmail.com>
2019-12-16 20:35:46 -0700

    Updated files

--- js/time.js
+++ js/time.js
@@ -1,31 +1,32 @@
 //Variable Declarations
-
-const nineAmText = $("#nineAm");
 const userInput = '';
-//Display the current date/time
+let currently = $("#currentDay");
 
-$("#currentDay").text(moment().format("dddd, MMMM Do, hh:mm a"));
+setInterval(() => { //This is the "fat arrow" function syntax -- this function makes sure that time moves
+    const now = moment(); //Starts the interval the moment page loads
+    const date = now.format("dddd, MMMM Do, hh:mm a");
+    currently.text(date);
+}, 1000);
 
+//When Save button is clicked ... 
+$(".saver").on("click", function() {
+    event.preventDefault();
+    console.log($(".namers").val());
+    //If clicked, make if st8ment that 
+            // - Checks 2 see if user input text into textarea in specific time slot 
+            //If yes,  store coinciding time & user's text input in2 localStorage
+    //If NOT clicked, create alert saying user must hit save -- MAY REMOVE THIS  IDEA
+//Have that localStorage information stay displayed, even with page refresh
 
-//This references the Save Button
-//Step 1 for Save Button: Make it clickable 
-//Step 2: Make an if statement to check for clicking 
-    //If clicked, make an if statement that FIRST checks to see if user input text into the textarea in a specific time slot 
-         //If they did input text, store the conciding time and user's text input into localStorage
-    //If NOT clicked, create an alert saying that the user must hit save -- MAY REMOVE THIS  IDEA
-//Step 4: Have that localStorage information stay displayed, even with page refresh
-
-$(".pumpkin").click(function() {
-alert($("input:text").val()); //Testing to see if this displays the text that the user input, which it does
 }); 
 
 
-//This references the text input field - we will need to make this respond to entries
+//Below code references text input field - need to make this respond to entries
 //$(".namers")
 
-//TimeBlocks -- must change colors based on time of day, need to use if statement for checks
+//TimeBlocks -- must change colors based on time of day, use if statement for checks
 
-    // const pastTimeBlock -- Create a check to see if the current timeblock events already happened, and if they have, make them green. If not, no color change.
+    // const pastTimeBlock -- Create check to see if current timeblock events already happened, and if have, make them green. If not, no color change.
     // const currentTimeBlock -- Create a check to see if the current timeblock events are happening at the present time, and if they are, have them be blue. If not, no color change.
     // const futureTimeBlock  -- Create a check to see if the future timeblock events are going to happen after the present time, and if they are, have them be red. If not, no color change.
     // const emptyTimeBlock // Create a check (if statement) to see if there's any TimeBlocks with no information. If no infromation, turn the text area gray. If not, no color change.
